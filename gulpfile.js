e9340cdb87e68d808c926d2c213928fd12ab674e
Alex Smith <alex.smith2844@gmail.com>
2020-04-05 11:27:12 +0500

    add build js_client

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,18 @@ const
 	closureCompiler = require('google-closure-compiler').gulp(),
 	httpServer = require('http-server');
 
-gulp.task('js', done => {
+gulp.task('js_client', done => {
+	let task = gulp.src([
+		'client.js'
+	]).pipe(plumber());
+	if (process.env.COMPILER != 'false')
+		task = task.pipe(closureCompiler({
+			compilation_level: 'SIMPLE',
+			js_output_file: 'client.js'
+		}, { platform: ['native', 'java', 'javascript'] }));
+	task.pipe(gulp.dest('dist/js')).on('end', () => done());
+});
+gulp.task('js_lib', done => {
 	let task = gulp.src([
 		'src/js/globalStorage.js'
 	]).pipe(plumber());
@@ -21,6 +32,7 @@ gulp.task('html', async () => {
 	if (process.env.DEV != 'true')
 		return;
 	httpServer.createServer({ cache: -1 }).listen(3000);
-	gulp.watch('src/js/**/*.js', gulp.series('js'));
+	gulp.watch('client.js', gulp.series('js_client'));
+	gulp.watch('src/js/**/*.js', gulp.series('js_lib'));
 });
-gulp.task('default', gulp.series('js', 'html'));
+gulp.task('default', gulp.series('js_client', 'js_lib', 'html'));
